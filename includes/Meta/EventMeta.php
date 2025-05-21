6df093eef7f0b62945dff1bbf873d6a671ecec84
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-05-21 15:26:57 -0600

    Cleancode

--- includes/Meta/EventMeta.php
+++ includes/Meta/EventMeta.php
@@ -5,7 +5,6 @@ class EventMeta extends AbstractMetaRegistrar {
     protected function get_post_type(): string {
         return 'event';
     }
-
     protected function get_fields(): array {
         return ['date', 'type', 'location', 'description'];
     }
