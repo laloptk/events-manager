402e2cb18ae190052a35d8f17e71b0e7d37293dd
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-05-23 20:21:32 -0600

    Delete files and refactor on the registering blocks to validate and place attributes correctly

--- includes/Blocks/Blocks.php
+++ includes/Blocks/Blocks.php
@@ -2,6 +2,7 @@
 namespace EventOps\Blocks;
 
 class Blocks {
+    protected const BLOCKS_DIR = WP_PLUGINS_DIR . '/eventops/build/blocks/';
 
     public function register() {
         $paths = $this->get_blocks_paths();
@@ -11,15 +12,18 @@ class Blocks {
     }
 
     protected function get_blocks_paths(): array {
-        // Assuming the blocks are located in the 'build/blocks/' directory of the plugin
-        $blocks_path = WP_PLUGIN_DIR . '/eventops/build/blocks/';
         $blocks_list = apply_filters(
             'eventos_modify_block_paths', 
             array(
-                $blocks_path . 'event-block/',
+                self::BLOCKS_DIR . 'event-block/',
             )
         );
+
+        // Validate: make sure each path is a directory and has a block.json
+        $valid_paths = array_filter( $blocks_list, function( $path ) {
+            return is_dir( $path ) && file_exists( $path . 'block.json' );
+        });
         
-        return $blocks_list;
+        return $valid_paths;
     }
 }
